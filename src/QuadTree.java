a5ff40c94ba55a523fc78408cd1a59878d1f0530
fwoodo <fwoodo@gmail.com>
2024-02-22 12:07:33 -0300

    basic functionality for QuadTree for testing nodes

--- src/QuadTree.java
+++ src/QuadTree.java
@@ -1,4 +1,14 @@
 
 public class QuadTree {
-
+	QuadNode root;
+	final static int WORLDVIEW=1024;
+	
+	public QuadTree() {
+		root = new LeafNode();
+	}
+	
+	public void insert(Point p) {
+		root.insert(p, 0, 0, WORLDVIEW);
+	}
 }
+
